77213c8d1b739956baf8a1a26ea34df19db80cd6
mattgclaudio <mc824@njit.edu>
2020-09-28 03:09:53 -0400

    Update loginScript.php

--- loginScript.php
+++ loginScript.php
@@ -7,7 +7,10 @@ require_once('rabbitMQLib.inc');
 
 function doLogin($username, $password)
 {
-    # this function needs to pass the parameters to the RabbitClient code which connects to the                     	# RabbitServer code running on the DB. This file needs to live in a separate directory, as it needs its own testRabbitMQ.ini file which points at the RabbitServer on the DB rather than the one on the RabbitMQ VM. The $response returned by this method is then passed back to Apache2 and printed to the screen as "logged in" or not logged in. 
+    # this function needs to pass the parameters to the RabbitClient code which connects to the                     
+    # RabbitServer code running on the DB. This file needs to live in a separate directory, 
+    # as it needs its own testRabbitMQ.ini file which points at the RabbitServer on the DB rather than the one on the RabbitMQ VM. 
+    #The $response returned by this method is then passed back to Apache2 and printed to the screen as "logged in" or not logged in. 
 $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
 $request = array();
 $request['type'] = "Login";
