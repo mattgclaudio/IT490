427daac3eeea56e3dc06940710573fd316462e11
mattgclaudio <mc824@njit.edu>
2020-10-03 23:32:10 -0400

    Update loggingClient.php

--- loggingClient.php
+++ loggingClient.php
@@ -4,10 +4,14 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
-# it seems like there is no harm in leaving the server running for days at a ti# me, and i cannot at this point see any benefit in trying to rewrite the work #  that at the professor has done creating the rabbitMQClient/Server objects in# Lib.inc, so i think it's better we not have to mess around with that base    #  code too much. 
-#
-# That being said, it would be highly beneficial to find a way to close client # connections in a way which is reflected in the management login for RabbitMQ,# or else we will have to leave those lines running forever..which 
-# seems wasteful. 
+# it seems like there is no harm in leaving the server running for days at a time, 
+# and i cannot at this point see any benefit in trying to rewrite the work 
+#  that at the professor has done creating the rabbitMQClient/Server objects in
+# Lib.inc, so i think it's better we not have to mess around with that base code too much. 
+# That being said, it would be highly beneficial to find a way to close client 
+# connections in a way which is reflected in the management login for RabbitMQ,
+# or else we will have to leave those lines running forever..which seems wasteful.
+
 
 function updateLog($errmsg) {
 	# with this a+ opening mode we APPEND to this existing logbook
@@ -30,7 +34,7 @@ function checkLog($rablog) {
 	#  %$&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 	# IF THIS IS RUNNING ON YOUR VM YOU MUST CHANGE THE HOME DIRECTORY
 	# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-	$ourlog = file_get_contents("home/matt/logbook.txt");
+	$ourlog = getLog();
 	$oursha = sha1($ourlog);
 	$rabsha = sha1($rablog);
 	if ($oursha != $rabsha):
