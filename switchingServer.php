f5e5cef550dd0af46103cd36c7a5ec57dc39d2ad
Matt Claudio <mattgclaudio@gmail.com>
2020-10-13 13:34:03 -0400

    test

--- switchingServer.php
+++ switchingServer.php
@@ -9,56 +9,69 @@ require_once('rabbitMQLib.inc');
 function callDB($unthree, $pwthree) {
 
 $client = new rabbitMQClient("db_server.ini","dbServer");
-if (isset($argv[1]))
-{
-  $msg = $argv[1];
-}
-else
-{
-  $msg = "test message";
-}
+
 
 $request = array();
 
 $request['type'] = "login";
 $request['username'] = $unthree;
 $request['password'] = $pwthree;
-$request['message'] = $msg;
+
 
 $response = $client->send_request($request);
+
+$_SESSION['pubkey'] = $response['pubkey'];
+$_SESSION['privkey'] = $response['privkey'];
 
 return $response;
 }
 
 
+function callDMZ($pkey, $prkey, $action) {
 
+        $client = new rabbitMQClient("dmzServer.ini","dmzServer");
+
+        $request = array();
+        $request['action'] =  $action;
+        $request['pubkey'] = $pkey;
+        $request['privkey'] = $prkey;
+
+        $response = $client->send_request($request);
+
+        return $response;
+
+}
 
 
 function requestProcessor($request)
 {
   echo "received request".PHP_EOL;
   var_dump($request);
-  $pass = "";
   if(!isset($request['type']))
   {
     return "ERROR: unsupported message type";
   }
+
   switch ($request['type'])
   {
-    case "login":
-	    $pass = callDB($request['username'],$request['password']);	    
+  	case "dmz1":
+		$retmsg = callDMZ($_SESSION['pubkey'], $_SESSION['privkey'], $request['action']);
+
+  	case "login":
+		$retmsg = callDB($request['username'],$request['password']);
+  		$retmsg = $retmsg['response'];		
 
   }
-  return array("returnCode" => '0', 'message'=>$pass['response'], 'pubkey' => $pass['pubkey'], 'privkey' => $pass['privkey']);
+  return array("returnCode" => '0', 'message'=>$retmsg);
 }
 
-$server = new rabbitMQServer("testRabbitMQ.ini","testServer");
+$server = new rabbitMQServer("webconn.ini","testServer");
 
 echo "Switching Server BEGIN".PHP_EOL;
 
 $server->process_requests('requestProcessor');
 
-echo "Server With apache2 END".PHP_EOL;
+echo "Switching server  END".PHP_EOL;
 
 exit();
 
